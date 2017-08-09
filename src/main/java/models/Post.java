a7309e5ede23ffe0cbbe3c5c363abc282392c5f7
Esti Shay and Kate Mashokha <esti@>
2017-08-09 09:58:13 -0700

    add ability to delete individual post

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -56,4 +56,8 @@ public class Post {
         this.content = content;
     }
 
+    public void deletePost(){
+        instances.remove(id-1);
+    }
+
 }
--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -91,6 +91,15 @@ public class PostTest {
         assertNotEquals(formerContent, post.getContent());
     }
 
+    @Test
+    public void deleteDeletesASpecificPost() throws Exception {
+        Post post = setupNewPost();
+        Post otherPost = new Post("How to pair successfully");
+        post.deletePost();
+        assertEquals(1, Post.getAll().size()); //one is left
+        assertEquals(Post.getAll().get(0).getId(), 2); //the one that was deleted has the id of 2. Why do we care?
+    }
+
     //helper
     public Post setupNewPost(){
         return new Post("Day 1: Intro");
